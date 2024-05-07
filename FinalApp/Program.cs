8299e18be36a4758b311df8168aadbffb18d6d5f
Marek <marekschroyer@gmail.com>
2024-05-07 08:43:31 -0600

    Some Final Edits

---
+++ FinalApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+while (true)
+{
+    try
+    {
+        Console.Write("Enter your birthdate: ");
+        DateTime birthDate = DateTime.Parse(Console.ReadLine());
+        int Days = (DateTime.Now.Year * 365 + DateTime.Now.DayOfYear) - (birthDate.Year * 365 + birthDate.DayOfYear);
+        int Years = Days / 365;
+        string message = (Days >= 365) ? " Your age: " + Years + " years" : "Your age: " + Days + " days";
+        Console.WriteLine(message);
+    }
+    catch
+    {
+        Console.WriteLine("You have entered an invalid date.\n");
+    }
+}
